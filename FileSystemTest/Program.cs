99597bcfbca08f68558c1886a518258873ba2aec
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2018-03-07 20:47:30 +0100

    Write method for creating new folder.

--- FileSystemTest/Program.cs
+++ FileSystemTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace FileSystemTest
     {
         static void Main(string[] args)
         {
+            var app = new Program();
+            app.CreateDirectory();
+
+            Console.ReadLine();
+        }
+
+        public void CreateDirectory()
+        {
+            string directoryName = "TestFolder";
+
+            if (Directory.Exists(path: directoryName))
+            {
+                Console.WriteLine($"Dir {directoryName} exists");
+            }
+            else
+            {
+                Directory.CreateDirectory(path: directoryName);
+                Console.WriteLine($"Create dir {directoryName}");
+            }
         }
     }
 }
